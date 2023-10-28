38526471647b1c577dd0140a666219379f2409be
abhikunu007 <ak0879918@gmail.com>
2023-10-28 16:51:59 +0530

    setup ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div className="main bg-dark vh-100">
+      <div className="top p-5">
+        <h3>IP Address Tracker</h3>
+        <form>
+        <input type="text" name='ip' id='ip' placeholder='Search IP' required className='mt-2 px-3 py-2 rounded'/>  
+        <button type='submit' className='btn btn-dark py-2 mb-1'>
+            <i className='fa fa-arrow-circle-right'></i>
+        </button>
+        </form> 
+        <div className='bg-white d-flex gap-4'>
+        <div className='border-end p-3'>
+        <p>IP ADDRESS</p>
+        <h4>19.0.0.7.7</h4>
+      </div> 
+
+      <div className='border-end p-3'>
+        <p>LOCATION</p>
+        <h4>Motihari</h4>
+      </div> 
+
+      <div className='border-end p-3'>
+        <p>TIMEZONE</p>
+        <h4>IST-7:00</h4>
+      </div> 
+
+      <div className=' p-3'>
+        <p>ISP</p>
+        <h4>Abhishek</h4>
+      </div> 
+        </div>
+         
+      </div>
+      
+      <div className="bottom container" style={{"color":"white"}}>
+        Bottom
+      </div>
     </div>
+   </>
   );
 }
 
