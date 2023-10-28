3b2f6cca20f6411dbd5a0fb0858e25566da2391a
abhikunu007 <ak0879918@gmail.com>
2023-10-28 21:44:50 +0530

    show data using api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css"
 import './App.css'
 import icon from './icon'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [address, setAddress] = useState(null);
+  const [ip, setIp] = useState("");
+  useEffect(() => {
+    try{
+      const getData = async() => {
+        const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_U9Ve6ZnzU67x1x2fKI7Jdzcq2OOok&ipAddress=190.211.173.101`)
+
+        const data = await res.json()
+        setAddress(data);
+        console.log(data);
+      }
+      getData()
+    } catch(error) {
+      console.trace(error);
+    }
+  }, [])
   return (
    <>
     <div className="main bg-dark vh-100">
@@ -16,43 +33,47 @@ function App() {
             <i className='fa fa-arrow-circle-right'></i>
         </button>
         </form> 
-        <div className='bg-white d-flex gap-4'>
+        {address && 
+          <div className='bg-white d-flex gap-4'>
+          <div className='border-end p-3'>
+          <p>IP ADDRESS</p>
+          <h4>{address.ip}</h4>
+        </div> 
+  
         <div className='border-end p-3'>
-        <p>IP ADDRESS</p>
-        <h4>19.0.0.7.7</h4>
-      </div> 
-
-      <div className='border-end p-3'>
-        <p>LOCATION</p>
-        <h4>Motihari</h4>
-      </div> 
-
-      <div className='border-end p-3'>
-        <p>TIMEZONE</p>
-        <h4>IST-7:00</h4>
-      </div> 
-
-      <div className=' p-3'>
-        <p>ISP</p>
-        <h4>Abhishek</h4>
-      </div> 
-        </div>
+          <p>LOCATION</p>
+          <h4>{address.location.city}, {address.location.region}</h4>
+        </div> 
+  
+        <div className='border-end p-3'>
+          <p>TIMEZONE</p>
+          <h4> UTC {address.timezone}</h4>
+        </div> 
+  
+        <div className=' p-3'>
+          <p>ISP</p>
+          <h4>{address.isp}</h4>
+        </div> 
+          </div>
+        }
          
       </div>
       
 
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{height:"370px", width:"100vw"}}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker icon={icon} position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-
+      {address && 
+        <MapContainer center={[address.location.lat, address.location.lng]} zoom={11} scrollWheelZoom={true} style={{height:"370px", width:"100vw"}}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker icon={icon} position={[address.location.lat, address.location.lng]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    
+      }
     </div>
    </>
   );
